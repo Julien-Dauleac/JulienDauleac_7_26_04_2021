386632411835421f84507435f5f723e93575f52c
Julien Dauleac <julien.dauleac@gmail.com>
2021-06-14 13:59:16 +0200

    Modification SCSS

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 
-// IMPORT AXIOS ET CONFIGURATION //
+// Import Axios et configuration //
 import axios from "axios";
 axios.defaults.baseURL = 'http://localhost:3000/api/';
 const token = sessionStorage.getItem('token');
