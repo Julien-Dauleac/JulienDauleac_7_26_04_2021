89f11dde715100a6d11a8e07ca47b69f817bcf6c
Julien Dauleac <julien.dauleac@gmail.com>
2021-06-15 09:42:07 +0200

    Modification comment

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -6,11 +6,11 @@ const router = express.Router();
 // Importe le controller //
 const postCtrl = require("../controllers/post");
 
-// On importe le middleware auth pour sécuriser les routes et le middleware multer pour la gestion des images
-const auth = require("../middleware/auth"); // Crée un token d'identification
-const multer = require("../middleware/multer-config"); // Permet d'envoyer un fichier dans la requête
+// On importe le middleware auth pour sécuriser les routes et le middleware multer pour la gestion des images //
+const auth = require("../middleware/auth"); // Crée un token d'identification //
+const multer = require("../middleware/multer-config"); // Permet d'envoyer un fichier dans la requête //
 
-// Création des différentes ROUTES de l'API en leurs précisant, dans l'ordre, leurs middlewares et controllers //
+// Création des différentes routes de l'API en leurs précisant, dans l'ordre, leurs middlewares et controllers //
 router.get("/", auth, postCtrl.getAllPosts);
 router.get("/:id", auth, postCtrl.getOnePost);
 router.post("/", auth, multer, postCtrl.createPost);
